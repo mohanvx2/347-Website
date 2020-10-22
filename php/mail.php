1171a7144b3ca41ca928ab78c04aa2d17774464a
Varun Mohan <vmohan030@gmail.com>
2020-10-21 20:24:51 -0400

    Modified form to be part of main footer

---
+++ php/mail.php
@@ -0,0 +1,36 @@
+<?php
+
+$webmaster_email = "vmohan030@gmail.com";
+
+
+$feedback_page = "index.html";
+$error_page = "index.html";
+$thankyou_page = "index.html";
+
+
+$email_address = $_REQUEST['email_address'] ;
+$comments = $_REQUEST['comments'] ;
+$first_name = $_REQUEST['first_name'] ;
+$msg = 
+"First Name: " . $first_name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Comments: " . $comments ;
+
+//Redirect them to the feedback form,
+if (!isset($_REQUEST['email_address'])) {
+header( "Location: $feedback_page" );
+}
+
+// Redirect to the error page.
+elseif (empty($first_name) || empty($email_address)) {
+header( "Location: $error_page" );
+}
+
+
+
+// Error check and send email
+else {
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+	header( "Location: $thankyou_page" );
+}
+?>
